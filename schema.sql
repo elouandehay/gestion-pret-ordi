04a72a25152de3a83276f2730d79b10da4e181ad
hugoldwz <hugo.ludowicz07@orange.fr>
2025-12-07 18:44:57 +0100

    Rajout de tables en SQL

--- schema.sql
+++ schema.sql
@@ -1,16 +1,19 @@
 DROP TABLE IF EXISTS ordinateurs;
 DROP TABLE IF EXISTS etudiants;
 DROP TABLE IF EXISTS prets;
+DROP TABLE IF EXISTS commentaires;
 
 
 CREATE TABLE etudiants (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nom TEXT NOT NULL,
     prenom TEXT NOT NULL,
-    email TEXT NOT NULL
+    email TEXT NOT NULL,
+    boursier BOOL NOT NULL
 );
 CREATE TABLE ordinateurs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date_sortie DATE NOT NULL,
     modele TEXT NOT NULL,
     dispo INTEGER DEFAULT 1
 );
@@ -24,6 +27,19 @@ CREATE TABLE prets (
     FOREIGN KEY (etudiant_id) REFERENCES etudiants(id),
     FOREIGN KEY (ordinateur_id) REFERENCES ordinateurs(id)
 );
+
+CREATE TABLE commentaires (
+   ordinateur_id INTEGER,
+   commentaire TEXT,
+   date_commentaire DATE NOT NULL,
+   FOREIGN KEY (ordinateur_id) REFERENCES ordinateurs(id)
+   );
+  
+CREATE TABLE compte (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   nom_utilisateur TEXT NOT NULL,
+   mdp_hashage TEXT NOT NULL
+   );
 
 -- On insère quelques PC pour tester
 INSERT INTO ordinateurs (nom, etat) VALUES ('PC-001', 'disponible');
